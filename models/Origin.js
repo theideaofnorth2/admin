d75c7c1d5e94b722a9a5e921d7d210ed4e43c75c
Vincent <vkammerer@gmail.com>
2017-09-24 20:36:37 -0400

    Removes requirement to add Lat and Lng to origins

--- models/Origin.js
+++ models/Origin.js
@@ -3,32 +3,43 @@ const Types = keystone.Field.Types;
 const { SuperPromise } = require('../utils/tools');
 
 const Origin = new keystone.List('Origin', {
-	autokey: { from: 'name', path: 'key', unique: true },
+  autokey: { from: 'name', path: 'key', unique: true },
 });
 
 Origin.add({
-	name: { type: String, required: true, index: true },
-	nativeName: { type: String, label: 'Native name' },
-	lat: { type: Number, required: true, default: 0 },
-	lng: { type: Number, required: true, default: 0 },
-	image: { type: String, default: '', label: 'Cover image file name' },
-	vertical: { type: Types.Select, label: 'Vertical alignment',
-		options: 'top, bottom', default: 'bottom',
-	},
-	horizontal: { type: Types.Select, label: 'Horizontal alignment',
-		options: 'left, center, right', default: 'center',
-	},
-	zoom: { type: Types.Select, numeric: true, label: 'Zoom level',
-		options: '13, 14', default: '13' },
+  name: { type: String, required: true, index: true },
+  nativeName: { type: String, label: 'Native name' },
+  lat: { type: Number, default: 0 },
+  lng: { type: Number, default: 0 },
+  image: { type: String, default: '', label: 'Cover image file name' },
+  vertical: {
+    type: Types.Select,
+    label: 'Vertical alignment',
+    options: 'top, bottom',
+    default: 'bottom',
+  },
+  horizontal: {
+    type: Types.Select,
+    label: 'Horizontal alignment',
+    options: 'left, center, right',
+    default: 'center',
+  },
+  zoom: {
+    type: Types.Select,
+    numeric: true,
+    label: 'Zoom level',
+    options: '13, 14',
+    default: '13',
+  },
 });
 
 Origin.schema.statics.getAll = () => {
-	const superPromise = SuperPromise();
-	Origin.model
-		.find({}, 'key name nativeName lat lng image vertical horizontal zoom')
-		.exec()
-		.then(superPromise.resolve);
-	return superPromise.promise;
+  const superPromise = SuperPromise();
+  Origin.model
+    .find({}, 'key name nativeName lat lng image vertical horizontal zoom')
+    .exec()
+    .then(superPromise.resolve);
+  return superPromise.promise;
 };
 
 Origin.defaultColumns = 'name, lat, lng';
